b4701bcb8717729a9f908422e026a15b781afe82
Samantha Kyle <samanthalkyle@gmail.com>
2026-02-06 15:13:12 -0500

    made it story points

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtLWt5bGUiLCJhIjoiY21rZTR3NW82MDNjazNscHdvZ
 
 const map = new mapboxgl.Map({
     container: 'my-map',
-    style: 'mapbox://styles/sam-kyle/cmkyjc7qx00bk01qu5wel21gt',
+
+    style: 'mapbox://styles/sam-kyle/cmlb9wo9h000d01qpewsvayee',
     //style: 'mapbox://styles/mapbox/standard',
-    center: [-79.39, 43.66], // starting position [lng, lat]
-    zoom: 12, // starting zoom
+    center: [-79.294541, 43.67431], 
+
+ // starting position [lng, lat]
+    zoom: 14, // starting zoom
 })
 
 map.on('load', () => {
@@ -13,54 +16,137 @@ map.on('load', () => {
     // Add a data source containing GeoJSON data
 
     // must add data sources here
-    map.addSource('uoft-data', {
-        type: 'geojson',
-        data: {
-            "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "properties": {
-                        "name": "Sidney Smith Hall"
-                    },
-                    "geometry": {
-                        "coordinates": [
-                            -79.39865237301687,
-                            43.662343395037766
-                        ],
-                        "type": "Point"
-                    }
+    map.addSource('places', {
+        'type': 'geojson',
+        'generateId': true,
+        'data': {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "properties": {
+                    "story": "had my first kiss here :)"
+                },
+                "geometry": {
+                    "coordinates": [
+                        -79.29544950231555,
+                        43.6665063925181
+                    ],
+                    "type": "Point"
                 }
-            ]
-        }
-    });
-    // Visualize data layer on map
-    map.addLayer({
-        'id': 'uoft-pnt',
-        'type': 'circle',
-        'source': 'uoft-data',
-        'paint': {
-            'circle-radius': 6,
-            'circle-color': '#B42222'
-        }
-    });
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "story": "Red Cardinals love it here!"
+                },
+                "geometry": {
+                    "coordinates": [
+                        -79.29376960288484,
+                        43.67908229503047
+                    ],
+                    "type": "Point"
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "story": "hidden gem for thrifting"
+                },
+                "geometry": {
+                    "coordinates": [
+                        -79.30443696426813,
+                        43.669362021919454
+                    ],
+                    "type": "Point"
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "story": "BOILER ROOM!!!!!"
+                },
+                "geometry": {
+                    "coordinates": [
+                        -79.31128255444673,
+                        43.66501776587799
+                    ],
+                    "type": "Point"
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "story": "The best place in the whole city for watching the sunset."
+                },
+                "geometry": {
+                    "coordinates": [
+                        -79.28732961641894,
+                        43.66947770995037
+                    ],
+                    "type": "Point"
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "story": "Good to visit for Family Day!"
+                },
+                "geometry": {
+                    "coordinates": [
+                        -79.2981517126696,
+                        43.67421445111728
+                    ],
+                    "type": "Point"
+                }
+            }
+        ]
+    }
+}
+);
+// Add a layer showing the places.
+map.addLayer({
+    'id': 'places',
+    'type': 'circle',
+    'source': 'places',
+    'paint': {
+        'circle-color': '#4264fb',
+        'circle-radius': 6,
+        'circle-stroke-width': 2,
+        'circle-stroke-color': '#ffffff'
+    },
+    'visible': false
+});
 
-    map.addSource('buildings-data', {
-        type: 'geojson',
-        //data: 'C:\Users\saman\Downloads\Github\GGR472\exercise5\wk5-data\buildings.geojson' // Your URL to your buildings.geojson file
-        // data: 'https://SamanthaKyle.github.io/exercise5/wk5-data/buildings.geojson'
-        //data: 'https://raw.githubusercontent.com/SamanthaKyle/exercise5/main/wk5-data/buildings.geojson'
-        //data: 'https://github.com/SamanthaKyle/exercise5/blob/6b5994d932f05a96f3fc345d500828a75371924e/wk5-data/buildings.geojson'
-        data: 'https://raw.githubusercontent.com/SamanthaKyle/exercise5/refs/heads/master/wk5-data/buildings.geojson'
-    });
-    map.addLayer({
-        'id': 'buildings-point',
-        'type': 'circle',
-        'source': 'buildings-data',
-        'paint': {
-            'circle-radius': 5,
-            'circle-color': '#007cbf'
-        }
-    });
+// Create the popup UI, but don't add it to the map yet.
+// You only want the UI to appear once the cursor is hovering over an element.
+const popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+});
+
+map.addInteraction('places-mouseenter-interaction', {
+    type: 'mouseenter',
+    target: { layerId: 'places' },
+    handler: (e) => {
+        map.getCanvas().style.cursor = 'pointer';
+
+        // Copy the coordinates from the POI underneath the cursor
+        const coordinates = e.feature.geometry.coordinates.slice();
+        const description = e.feature.properties.story;
+
+        // Populate the popup and set its coordinates based on the feature found.
+        popup.setLngLat(coordinates).setHTML(description).addTo(map);
+    }
+});
+
+map.addInteraction('places-mouseleave-interaction', {
+    type: 'mouseleave',
+    target: { layerId: 'places' },
+    handler: () => {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+    }
+});
 });
 
